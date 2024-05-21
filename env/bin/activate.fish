3e7f5ac9b7f83cb3faf468bb8a651a32ae92e2db
robin <robin.rajan@peerxp.com>
2024-05-21 12:03:43 +0530

    troubleshoot

--- env/bin/activate.fish
+++ env/bin/activate.fish
@@ -57,7 +57,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV '/home/projects/django/teak_wood/env'
+set -gx VIRTUAL_ENV '/home/projects/django/teak/teak_wood_backend/env'
 
 # https://github.com/fish-shell/fish-shell/issues/436 altered PATH handling
 if test (echo $FISH_VERSION | head -c 1) -lt 3
